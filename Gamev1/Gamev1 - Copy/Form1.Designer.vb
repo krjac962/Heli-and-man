0969d7d166854aea9222d12ce1b3d7213b442507
Kyle Jackson <75323683+krjac962@users.noreply.github.com>
2020-12-03 08:08:29 -0700

    Add files via upload
    Whole game visual studio files, 49

---
+++ Gamev1/Gamev1 - Copy/Form1.Designer.vb
@@ -0,0 +1,261 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4wall = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7wall = New System.Windows.Forms.PictureBox()
+        Me.Helicopter = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5wall = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6wall = New System.Windows.Forms.PictureBox()
+        Me.bullet = New System.Windows.Forms.PictureBox()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer4 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer5 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1a = New System.Windows.Forms.PictureBox()
+        Me.Timer6 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer7 = New System.Windows.Forms.Timer(Me.components)
+        Me.FileSystemWatcher1 = New System.IO.FileSystemWatcher()
+        Me.Timer8 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer9 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer10 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4wall, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7wall, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Helicopter, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5wall, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6wall, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.bullet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1a, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(39, 352)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(34, 68)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(-3, 224)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(810, 214)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 1
+        Me.PictureBox3.TabStop = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(499, 121)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(73, 76)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 2
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox4wall
+        '
+        Me.PictureBox4wall.Image = CType(resources.GetObject("PictureBox4wall.Image"), System.Drawing.Image)
+        Me.PictureBox4wall.Location = New System.Drawing.Point(-27, 426)
+        Me.PictureBox4wall.Name = "PictureBox4wall"
+        Me.PictureBox4wall.Size = New System.Drawing.Size(834, 27)
+        Me.PictureBox4wall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4wall.TabIndex = 3
+        Me.PictureBox4wall.TabStop = False
+        '
+        'PictureBox7wall
+        '
+        Me.PictureBox7wall.Image = CType(resources.GetObject("PictureBox7wall.Image"), System.Drawing.Image)
+        Me.PictureBox7wall.Location = New System.Drawing.Point(-27, -37)
+        Me.PictureBox7wall.Name = "PictureBox7wall"
+        Me.PictureBox7wall.Size = New System.Drawing.Size(851, 98)
+        Me.PictureBox7wall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox7wall.TabIndex = 4
+        Me.PictureBox7wall.TabStop = False
+        '
+        'Helicopter
+        '
+        Me.Helicopter.Image = CType(resources.GetObject("Helicopter.Image"), System.Drawing.Image)
+        Me.Helicopter.Location = New System.Drawing.Point(186, 110)
+        Me.Helicopter.Name = "Helicopter"
+        Me.Helicopter.Size = New System.Drawing.Size(172, 96)
+        Me.Helicopter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.Helicopter.TabIndex = 5
+        Me.Helicopter.TabStop = False
+        '
+        'PictureBox5wall
+        '
+        Me.PictureBox5wall.Image = CType(resources.GetObject("PictureBox5wall.Image"), System.Drawing.Image)
+        Me.PictureBox5wall.Location = New System.Drawing.Point(788, 0)
+        Me.PictureBox5wall.Name = "PictureBox5wall"
+        Me.PictureBox5wall.Size = New System.Drawing.Size(19, 438)
+        Me.PictureBox5wall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox5wall.TabIndex = 6
+        Me.PictureBox5wall.TabStop = False
+        '
+        'PictureBox6wall
+        '
+        Me.PictureBox6wall.Image = CType(resources.GetObject("PictureBox6wall.Image"), System.Drawing.Image)
+        Me.PictureBox6wall.Location = New System.Drawing.Point(-3, 0)
+        Me.PictureBox6wall.Name = "PictureBox6wall"
+        Me.PictureBox6wall.Size = New System.Drawing.Size(19, 438)
+        Me.PictureBox6wall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox6wall.TabIndex = 7
+        Me.PictureBox6wall.TabStop = False
+        '
+        'bullet
+        '
+        Me.bullet.Image = CType(resources.GetObject("bullet.Image"), System.Drawing.Image)
+        Me.bullet.Location = New System.Drawing.Point(177, 266)
+        Me.bullet.Name = "bullet"
+        Me.bullet.Size = New System.Drawing.Size(19, 17)
+        Me.bullet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.bullet.TabIndex = 8
+        Me.bullet.TabStop = False
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        '
+        'Timer3
+        '
+        Me.Timer3.Enabled = True
+        Me.Timer3.Interval = 50
+        '
+        'Timer4
+        '
+        Me.Timer4.Enabled = True
+        '
+        'Timer5
+        '
+        Me.Timer5.Enabled = True
+        '
+        'PictureBox1a
+        '
+        Me.PictureBox1a.Image = CType(resources.GetObject("PictureBox1a.Image"), System.Drawing.Image)
+        Me.PictureBox1a.Location = New System.Drawing.Point(51, 168)
+        Me.PictureBox1a.Name = "PictureBox1a"
+        Me.PictureBox1a.Size = New System.Drawing.Size(34, 68)
+        Me.PictureBox1a.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1a.TabIndex = 9
+        Me.PictureBox1a.TabStop = False
+        Me.PictureBox1a.Visible = False
+        '
+        'Timer6
+        '
+        Me.Timer6.Enabled = True
+        '
+        'Timer7
+        '
+        Me.Timer7.Enabled = True
+        '
+        'FileSystemWatcher1
+        '
+        Me.FileSystemWatcher1.EnableRaisingEvents = True
+        Me.FileSystemWatcher1.SynchronizingObject = Me
+        '
+        'Timer8
+        '
+        Me.Timer8.Enabled = True
+        '
+        'Timer9
+        '
+        Me.Timer9.Enabled = True
+        '
+        'Timer10
+        '
+        Me.Timer10.Enabled = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox1a)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Helicopter)
+        Me.Controls.Add(Me.bullet)
+        Me.Controls.Add(Me.PictureBox6wall)
+        Me.Controls.Add(Me.PictureBox5wall)
+        Me.Controls.Add(Me.PictureBox7wall)
+        Me.Controls.Add(Me.PictureBox4wall)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4wall, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7wall, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Helicopter, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5wall, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6wall, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.bullet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1a, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox4wall As PictureBox
+    Friend WithEvents PictureBox7wall As PictureBox
+    Friend WithEvents Helicopter As PictureBox
+    Friend WithEvents PictureBox5wall As PictureBox
+    Friend WithEvents PictureBox6wall As PictureBox
+    Friend WithEvents bullet As PictureBox
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Timer3 As Timer
+    Friend WithEvents Timer4 As Timer
+    Friend WithEvents Timer5 As Timer
+    Friend WithEvents PictureBox1a As PictureBox
+    Friend WithEvents Timer6 As Timer
+    Friend WithEvents Timer7 As Timer
+    Friend WithEvents FileSystemWatcher1 As IO.FileSystemWatcher
+    Friend WithEvents Timer8 As Timer
+    Friend WithEvents Timer9 As Timer
+    Friend WithEvents Timer10 As Timer
+End Class
---
+++ Gamev1/Gamev1 - Copy/Form1.vb
@@ -0,0 +1,219 @@
+﻿Public Class Form1
+    Dim p As PictureBox
+    Dim t As Integer
+    Dim bloc As String
+    Dim direc As String
+    Dim pic1lo As String
+    Dim pic1a As String
+    Dim r As New Random
+    Dim helim As String
+    Dim score As Integer
+    Dim collision1a4 As String
+    Dim gravity As String
+    Dim pic1 As String
+
+
+    Sub Start()
+        collision1a4 = "f"
+        gravity = "t"
+    End Sub
+
+
+
+    Sub randmove(p As PictureBox)
+        Dim x As Integer
+        Dim y As Integer
+        MoveTo(p, x, y)
+        x = r.Next(-10, 11)
+        y = r.Next(-10, 11)
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        PictureBox1a.Location = PictureBox1.Location
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+
+        Select Case e.KeyCode
+
+            Case Keys.Up
+                MoveTo(PictureBox1, 0, 0)
+                    Case Keys.Down
+                MoveTo(PictureBox1, 0, 0)
+            Case Keys.Left
+                    MoveTo(PictureBox1, -5, 0)
+                    Case Keys.Right
+                    MoveTo(PictureBox1, 5, 0)
+                    Case Keys.W
+                    MoveTo(PictureBox1, 0, 0)
+                    Case Keys.S
+                    MoveTo(PictureBox1, 0, 0)
+                    Case Keys.A
+                    MoveTo(PictureBox1, -5, 0)
+                    Case Keys.D
+                    MoveTo(PictureBox1, 5, 0)
+                    Case Keys.F
+                PictureBox1a.Location = PictureBox1.Location
+                PictureBox1a.Visible = True
+                bullet.Location = PictureBox1.Location
+                bloc = "1"
+                MoveTo(PictureBox1, -2, -4)
+                MoveTo(PictureBox1, -2, 4)
+                PictureBox1.Refresh()
+                pic1a = "t"
+
+            Case Keys.Space
+                MoveTo(PictureBox1, 0, -10)
+                MoveTo(PictureBox1, 0, -10)
+                MoveTo(PictureBox1, 0, -10)
+                MoveTo(PictureBox1, 0, -10)
+                MoveTo(PictureBox1, 0, -10)
+
+            Case Keys.H
+                helim = "t"
+
+
+        End Select
+    End Sub
+    Sub MoveTo(p As PictureBox, distx As Integer, disty As Integer)
+        If IsClear(p, distx, disty, "WALL") Then
+            p.Location += New Point(distx, disty)
+        End If
+
+        If p.Name = "PictureBox1" And Collision(p, "WIN") Then
+            Me.BackColor = Color.Green
+            Return
+        Else
+            Me.BackColor = Color.White
+        End If
+
+    End Sub
+    Function IsClear(p As PictureBox, distx As Integer, disty As Integer, t As String) As Boolean
+        Dim b As Boolean
+
+        p.Location += New Point(distx, disty)
+        b = Not Collision(p, t)
+        p.Location -= New Point(distx, disty)
+        Return b
+    End Function
+
+
+
+    Function Collision(p As PictureBox, t As String)
+        Dim col As Boolean
+
+        For Each c In Controls
+            Dim obj As Control
+            obj = c
+            If p.Bounds.IntersectsWith(obj.Bounds) And (obj.Name.ToUpper.EndsWith(t) Or
+obj.Name.ToUpper.StartsWith(t)) And obj.Visible Then
+                col = True
+            End If
+        Next
+        Return col
+    End Function
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+
+        Static headstart As Integer
+        Static c As New Collection
+        c.Add(PictureBox1.Location)
+        headstart = headstart + 1
+        If headstart > 10 Then
+            p.Location = c.Item(1)
+            c.Remove(1)
+        End If
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub PictureBox5_Click(sender As Object, e As EventArgs) Handles bullet.Click
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+
+        If bloc = "1" Then
+            MoveTo(bullet, 150, 0.5)
+        End If
+
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        If gravity = "t" Then
+            MoveTo(PictureBox1, 0, 6)
+            MoveTo(PictureBox1, 0, 6)
+            MoveTo(PictureBox1, 0, 6)
+            MoveTo(PictureBox1, 0, 6)
+        End If
+
+
+    End Sub
+
+    Private Sub Timer4_Tick(sender As Object, e As EventArgs) Handles Timer4.Tick
+
+        MoveTo(PictureBox2, 0, 6)
+        MoveTo(PictureBox2, 0, 6)
+        MoveTo(PictureBox2, 0, 6)
+        MoveTo(PictureBox2, 0, 6)
+    End Sub
+
+    Private Sub Timer5_Tick(sender As Object, e As EventArgs) Handles Timer5.Tick
+        If pic1a = "t" Then
+            PictureBox1a.Visible = False
+            pic1a = "f"
+
+        End If
+    End Sub
+
+    Private Sub PictureBox1a_Click(sender As Object, e As EventArgs) Handles PictureBox1a.Click
+
+    End Sub
+
+    Private Sub Timer6_Tick(sender As Object, e As EventArgs) Handles Timer6.Tick
+        If bullet.Location = PictureBox2.Location Then
+            MoveTo(PictureBox2, 3, -1)
+            PictureBox2.Image.RotateFlip(RotateFlipType.Rotate180FlipNone)
+
+        End If
+    End Sub
+
+    Private Sub Timer7_Tick(sender As Object, e As EventArgs) Handles Timer7.Tick
+        If helim = "t" Then
+            MoveTo(Helicopter, 0.1, 10)
+            If PictureBox1.Bounds.IntersectsWith(Helicopter.Bounds) Then
+                MoveTo(PictureBox1, -1, -70)
+                PictureBox1.Visible = False
+                MoveTo(Helicopter, -0.1, -20)
+            End If
+
+        End If
+        PictureBox1.Visible = True
+    End Sub
+
+    Private Sub FileSystemWatcher1_Changed(sender As Object, e As IO.FileSystemEventArgs) Handles FileSystemWatcher1.Changed
+    End Sub
+
+    Private Sub Timer8_Tick(sender As Object, e As EventArgs) Handles Timer8.Tick
+        If collision1a4 = "t" Then
+            Timer9.Enabled = False
+            MoveTo(Helicopter, -0.1, -10)
+            MoveTo(PictureBox1, -0.1, -70)
+
+        End If
+    End Sub
+
+    Private Sub Timer9_Tick(sender As Object, e As EventArgs) Handles Timer9.Tick
+        gravity = "t"
+    End Sub
+
+    Private Sub Timer10_Tick(sender As Object, e As EventArgs) Handles Timer10.Tick
+        If collision1a4 = "d" Then
+            collision1a4 = "f"
+            Timer9.Enabled = True
+            helim = "f"
+        End If
+    End Sub
+End Class
